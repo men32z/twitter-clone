d076534264ce047c893c54ced6eea637521ca5c3
Luis Preza <preza.luis@gmail.com>
2020-06-06 12:45:51 -0500

    initial install, devise, rspect, and other usefull gems

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations model' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:password) }
+    it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+    it { should validate_uniqueness_of(:username)}
+
+    it 'should work with factory.' do
+      user = FactoryBot.create(:user)
+      expect(user).to be_valid
+    end
+  end
+end
